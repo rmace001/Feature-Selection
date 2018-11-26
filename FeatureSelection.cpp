1eb1bc8950300d1080c745b7a4421354e4ae2917
Rogelio Macedo <rmace001@ucr.edu>
2018-11-26 19:35:44 +0000

    Test Opening and Reading IEEE 754-1985 Single Precision Data

--- FeatureSelection.cpp
+++ FeatureSelection.cpp
@@ -3,27 +3,31 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+
+
 int main(int argc, char** argv)
 {
-  vector<float> Numericaldata;
+  vector<float> numericalData;
   vector<string> data;
   ifstream fin; 
   fin.open(argv[1]);
-  string temp; 
+  float temp; 
   if (!fin.is_open()){
     cout << "Could not open file: " << argv[1] << endl;
   }
   else {
     while (fin >> temp){
-      data.push_back(temp);
+      numericalData.push_back(temp);
+    }
+  
+    cout << "Read test" << endl;
+    cout << "Data Values read: "<< numericalData.size() << endl;
+    for (short i=0; i<numericalData.size(); i++){
+      cout << "Data at " << i+1 << ": " << numericalData[i] << endl;
+      
     }
   }
-  
-  
-  float n = -1.5841842e+00;
-  float n2 = 2*n;
-  cout << n2 << endl;
-  
   
   
   
