b3520b55364ecfe8e8f3d4d8a3f183af55e2c6b8
Rogelio Macedo <rmace001@ucr.edu>
2018-11-26 19:51:32 +0000

    Modularize Reading the Data into a Function

--- FeatureSelection.cpp
+++ FeatureSelection.cpp
@@ -3,37 +3,39 @@
 #include <fstream>
 #include <string>
 using namespace std;
+bool readData(string filename);
 
+bool readData(string filename, vector<float>& vec){
+  ifstream fin; 
+  fin.open(filename);
+  float temp; 
+  if (!fin.is_open()){
+    cout << "Could not open file: " << filename << endl;
+    return false;
+  }
+  else {
+    while (fin >> temp){
+      vec.push_back(temp);
+    }
+  }
+  fin.close();
+  return true;
+}
 
 
 int main(int argc, char** argv)
 {
   vector<float> numericalData;
   vector<string> data;
-  ifstream fin; 
-  fin.open(argv[1]);
-  float temp; 
-  if (!fin.is_open()){
-    cout << "Could not open file: " << argv[1] << endl;
-  }
-  else {
-    while (fin >> temp){
-      numericalData.push_back(temp);
-    }
+  if (!readData(argv[1], numericalData))
+    return 0;
   
-    cout << "Read test" << endl;
-    cout << "Data Values read: "<< numericalData.size() << endl;
-    for (short i=0; i<numericalData.size(); i++){
-      cout << "Data at " << i+1 << ": " << numericalData[i] << endl;
-      
-    }
+  cout << "Read test" << endl;
+  cout << "Data Values read: "<< numericalData.size() << endl;
+  for (short i=0; i<numericalData.size(); i++){
+    cout << "Data at " << i+1 << ": " << numericalData[i] << endl;
+    
   }
   
-  
-  
-  
-  
-  
-  fin.close();
   return 0;
 }
