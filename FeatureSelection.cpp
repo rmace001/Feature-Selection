45ecf98fad27b1949a87f95591dcbd65c46e8587
Rogelio Macedo <rmace001@ucr.edu>
2018-12-12 08:58:57 +0000

    Re-commit to be safe

--- FeatureSelection.cpp
+++ FeatureSelection.cpp
@@ -150,12 +150,6 @@ void featureIsolate(vector<int>& tmpFeatureSet, vector<vector<double>>& zeroedFe
     }
   }
   
-  // for (int i=0; i<5; i++){
-  //   for (int j=0; j<10; j++){
-  //     cout << zeroedFeatures[i][j] << "   ";  
-  //   }
-  //   cout << endl;
-  // }
 }
 
 
